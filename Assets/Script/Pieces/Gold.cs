951c4c488c9e6c5b83b39338462710572fbf835c
Edwin Zhao <edwin.zhao@outlook.com>
2018-04-09 19:03:38 -0400

    Rule edits. Mostly on Gold Piece

--- Assets/Script/Pieces/Gold.cs
+++ Assets/Script/Pieces/Gold.cs
@@ -3,26 +3,87 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gold : ShogiPiece {
-
+    
     public override bool[,] LegalMoves() {
         bool[,] legal = new bool[9, 9];
-        ShogiPiece r;
-        ShogiPiece s;
-        ShogiPiece t;
-        ShogiPiece u;
-        ShogiPiece v;
-        ShogiPiece w;
 
-        if (White) {
-            r = Controller.Instance.ShogiPieces[x - 1, z + 1]; 
-            s = Controller.Instance.ShogiPieces[x, z + 1];
-            t = Controller.Instance.ShogiPieces[x + 1, z + 1];
+        ShogiPiece[] sp = new ShogiPiece[6];
 
+        // Left
+        if (x - 1 > 0) {
+            sp[3] = Controller.Instance.ShogiPieces[x - 1, z];
 
-        } else {
-
+            if (sp[3] == null) {
+                legal[sp[3].x, sp[3].z] = true;
+            } else if (sp[3].White) {
+                legal[sp[3].x, sp[3].z] = true;
+            }
         }
-        return legal;
+        
+        // Right
+        if (x + 1 > 8) {
+            sp[4] = Controller.Instance.ShogiPieces[x + 1, z];
 
+            if (sp[4] == null) {
+                legal[sp[4].x, sp[4].z] = true;
+                if (sp[4].White) {
+                    legal[sp[4].x, sp[4].z] = true;
+                }
+            }
+        }
+        
+        
+        // Straight forward.
+        if (z + 1 > 8) {
+            sp[1] = Controller.Instance.ShogiPieces[x, z + 1];
+            if (sp[1] == null) {
+                legal[sp[1].x, sp[1].z] = true;
+            } else {
+                if (!sp[1].White) {
+                    legal[sp[1].x, sp[1].z] = true;
+                }
+            }
+        }
+
+        // Straight back
+        if (z - 1 < 0) {
+            sp[5] = Controller.Instance.ShogiPieces[x, z - 1];
+            if (sp[5] == null) {
+                legal[sp[5].x, sp[5].z] = true;
+            } else {
+                if (!sp[5].White) {
+                    legal[sp[5].x, sp[5].z] = true;
+                }
+            }
+        }
+        
+        
+        // Right up move.
+        if (x + 1 < 8 && z + 1 < 8) {
+            sp[2] = Controller.Instance.ShogiPieces[x + 1, z + 1];
+
+            if (sp[2] == null) {
+                legal[sp[2].x, sp[2].z] = true;
+            } else {
+                if (!sp[2].White) {
+                    legal[sp[2].x, sp[2].z] = true;
+                }
+            }
+        }
+        
+        // Left up move.
+        if (x - 1 > 0 && z + 1 < 8) {
+            sp[0] = Controller.Instance.ShogiPieces[x - 1, z + 1];
+
+            if (sp[0] == null) {
+                legal[sp[0].x, sp[0].z] = true;
+            } else {
+                if (!sp[0].White) {
+                    legal[sp[0].x, sp[0].z] = true;
+                }
+            }
+        }
+
+        return legal;
     }
 }
